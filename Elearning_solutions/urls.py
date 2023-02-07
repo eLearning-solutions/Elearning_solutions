99b242f2c1f51b7e176755296b1f5ec23770b4a5
Ubuntu <ubuntu@ip-172-31-94-94.ec2.internal>
2023-02-07 15:19:21 +0000

    prod setup

--- Elearning_solutions/urls.py
+++ Elearning_solutions/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('blog', include('blog.urls')),
     path('shop', include('shop.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ staticfiles/js/vanta.js
@@ -0,0 +1,24 @@
+//
+// import * as THREE from "three"
+// import FOG from "vanta/dist/vanta.fog.min"
+//
+// const section = document.getElementById('#hero')
+//
+//
+// let speed = 1.5
+//
+// const effect = FOG({
+//   el: section,
+//   THREE,
+//   mouseControls: false,
+//   touchControls: false,
+//   gyroControls: false,
+//   minHeight: 200.0,
+//   minWidth: 200.0,
+//   highlightColor: 0xca00d4,
+//   midtoneColor: 0xff0000,
+//   lowlightColor: 0xffed00,
+//   baseColor: 0x1c0036,
+//   blurFactor: 0.8,
+//   speed: 1.5,
+// })
