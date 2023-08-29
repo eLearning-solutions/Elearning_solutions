b3803418f7855e19c53b5e41f85fd2378d6a3be7
Clemo97 <lumumbaclement@gmail.com>
2023-08-29 17:06:35 +0300

    added riara and extra services offered

--- Elearning_solutions/settings.py
+++ Elearning_solutions/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATICFILES_DIRS = [BASE_DIR / 'core/static', ]
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
